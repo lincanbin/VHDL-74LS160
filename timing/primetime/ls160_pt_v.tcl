8f3a7873ac1c99294971ebcbcd747739977f834f
lincanbin <lincanbin@hotmail.com>
2015-04-27 00:39:34 +0800

    Quartus 9.0 Project of 74LS160.

---
+++ timing/primetime/ls160_pt_v.tcl
@@ -0,0 +1,92 @@
+## Copyright (C) 1991-2009 Altera Corporation
+## Your use of Altera Corporation's design tools, logic functions 
+## and other software and tools, and its AMPP partner logic 
+## functions, and any output files from any of the foregoing 
+## (including device programming or simulation files), and any 
+## associated documentation or information are expressly subject 
+## to the terms and conditions of the Altera Program License 
+## Subscription Agreement, Altera MegaCore Function License 
+## Agreement, or other applicable license agreement, including, 
+## without limitation, that your use is for the sole purpose of 
+## programming logic devices manufactured by Altera and sold by 
+## Altera or its authorized distributors.  Please refer to the 
+## applicable agreement for further details.
+
+## VENDOR "Altera"
+## PROGRAM "Quartus II"
+## VERSION "Version 9.0 Build 235 06/17/2009 Service Pack 2 SJ Web Edition"
+
+## DATE "04/27/2015 00:24:19"
+
+## 
+## Device: Altera EP1K50QC208-3 Package PQFP208
+## 
+
+## 
+## This Tcl script should be used for PrimeTime (Verilog) only
+## 
+
+## This file can be sourced in primetime
+
+set report_default_significant_digits 3
+set hierarchy_separator .
+
+set quartus_root "c:/quartus90sp2portable/app/quartus/altera/90sp2/quartus/"
+set search_path [list . [format "%s%s" $quartus_root "eda/synopsys/primetime/lib"]  ]
+
+set link_path [list *  flex10ke_asynch_io_lib.db  flex10ke_lcell_register_lib.db  flex10ke_asynch_lcell_lib.db  flex10ke_asynch_mem_lib.db  flex10ke_pll_lib.db  alt_vtl.db]
+
+read_verilog  flex10ke_all_pt.v 
+
+##########################
+## DESIGN ENTRY SECTION ##
+##########################
+
+read_verilog  ls160.vo
+current_design ls160
+link
+## Set variable timing_propagate_single_condition_min_slew to false only for versions 2004.06 and earlier
+regexp {([1-9][0-9][0-9][0-9]\.[0-9][0-9])} $sh_product_version dummy version
+if { [string compare "2004.06" $version ] != -1 } {
+   set timing_propagate_single_condition_min_slew false
+}
+set_operating_conditions -analysis_type single
+read_sdf ls160_v.sdo
+
+################################
+## TIMING CONSTRAINTS SECTION ##
+################################
+
+
+## Start clock definition ##
+# WARNING:  The required clock period is not set. The default value (100 ns) is used. 
+create_clock -period 100.000 -waveform {0.000 50.000} [get_ports { clk } ] -name clk  
+
+set_propagated_clock [all_clocks]
+## End clock definition ##
+
+## Start create collections ##
+## End create collections ##
+
+## Start global settings ##
+## End global settings ##
+
+## Start collection commands definition ##
+
+## End collection commands definition ##
+
+## Start individual pin commands definition ##
+## End individual pin commands definition ##
+
+## Start Output pin capacitance definition ##
+## End Output pin capacitance definition ##
+
+## Start clock uncertainty definition ##
+## End clock uncertainty definition ##
+
+## Start Multicycle and Cut Path definition ##
+## End Multicycle and Cut Path definition ##
+
+## Destroy Collections ##
+
+update_timing
