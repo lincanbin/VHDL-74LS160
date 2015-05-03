2212a0a55f6749d750c8f48623253f1d3235009f
lincanbin <lincanbin@hotmail.com>
2015-05-04 00:13:07 +0800

    Update

--- ls160.vhd
+++ ls160.vhd
@@ -1,45 +1,46 @@
-library ieee;
-use ieee.std_logic_1164.all; 
+LIBRARY ieee;
+USE ieee.std_logic_1164.all; 
 use ieee.std_logic_unsigned.all; 
  
-entity ls160 is 
-port(
+ENTITY ls160 is 
+PORT(
 	data: in std_logic_vector(3 downto 0);--4位预置数，load非高电平置数
 	clk,load,enp,ent,clr:in std_logic;--注释见architecture
-	count: buffer std_logic_vector(3 downto 0);--4位计数值，使用buffer让其保持状态
-	tc:out std_logic--溢出位，高电平溢出
+	q: buffer std_logic_vector(3 downto 0);--4位计数值，使用buffer让其保持状态
+	rco:out std_logic--溢出位，高电平溢出
 );
-end ls160; 
+END ls160; 
  
-architecture behavior of ls160 is 
-begin   
-	tc<='1' when (count="1001" and enp='1' and ent='1' and load='1' and clr='1') else '0';--溢出进位 
+architecture behavior OF ls160 IS 
+BEGIN   
+	rco<='1' when (q="1001" and enp='1' and ent='1' and load='1' and clr='1') else '0';--溢出进位 
 	process(clk,clr,enp,ent,load) begin 
 			if(rising_edge(clk)) then --时钟上升沿时开始工作
 				if(clr='1')then --CLR高电平工作，低电平清零
 					if(load='1')then --load非高电平置数
 						if(enp='1')then --EnableP，PT同时非高电平保存状态
 							 if(ent='1')then --EnableT，开关，PT同时非高电平保存状态
-								if(count="1001")then --Count为9，溢出重置
-									count<="0000"; 
+								if(q="1001")then --Count为9，溢出重置
+									q<="0000"; 
 								else  
-									count<=count+1; 
+									q<=q+1; 
 								end if; 
 							else
-								count<=count;
+								q<=q;
 							end if; 
 						else
-							count<=count;
+							q<=q;
 						end if; 
 					else
-						count<=data;
+						q<=data;
 					end if;
 				else
-					count<="0000";
+					q<="0000";
 				end if;
 			end if;
 	end process;
-end behavior;
+END behavior;
 
+--林灿斌
 --https://github.com/lincanbin
 --20150427
--- timing/primetime/ls160_pt_v.tcl
+++ timing/primetime/ls160_pt_v.tcl
@@ -1,8 +1,11 @@
 set report_default_significant_digits 3
 set hierarchy_separator .
+
 set quartus_root "c:/quartus90sp2portable/app/quartus/altera/90sp2/quartus/"
 set search_path [list . [format "%s%s" $quartus_root "eda/synopsys/primetime/lib"]  ]
+
 set link_path [list *  flex10ke_asynch_io_lib.db  flex10ke_lcell_register_lib.db  flex10ke_asynch_lcell_lib.db  flex10ke_asynch_mem_lib.db  flex10ke_pll_lib.db  alt_vtl.db]
+
 read_verilog  flex10ke_all_pt.v 
 read_verilog  ls160.vo
 current_design ls160
@@ -14,5 +17,7 @@ if { [string compare "2004.06" $version ] != -1 } {
 set_operating_conditions -analysis_type single
 read_sdf ls160_v.sdo
 create_clock -period 100.000 -waveform {0.000 50.000} [get_ports { clk } ] -name clk  
+
 set_propagated_clock [all_clocks]
+
 update_timing
